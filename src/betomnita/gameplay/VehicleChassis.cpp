e598648f64f522d6fdb841e756b8500f8d534aac
Adam Krause <adam.krause@huuugegames.com>
2019-02-20 11:06:13 +0100

    Vechicle Implementation Begin

--- src/betomnita/gameplay/VehicleChassis.cpp
+++ src/betomnita/gameplay/VehicleChassis.cpp
@@ -1,1 +1,24 @@
 #include "betomnita/gameplay/VehicleChassis.hpp"
+
+namespace Betomnita::GamePlay
+{
+    VehicleChassis::VehicleChassis()
+    {
+    }
+
+    VehicleChassis::~VehicleChassis()
+    {
+    }
+
+    void VehicleChassis::Render( sf::RenderTarget& target )
+    {
+    }
+
+    void VehicleChassis::Update( const sf::Time& dt )
+    {
+    }
+
+    void VehicleChassis::LoadFromPrototype( const VehicleChassisPrototype& prototype )
+    {
+    }
+}
--- src/betomnita/gameplay/VehicleChassis.hpp
+++ src/betomnita/gameplay/VehicleChassis.hpp
@@ -3,9 +3,23 @@
 
 namespace Betomnita::GamePlay
 {
+    class VehicleChassisPrototype;
+
     class VehicleChassis : public PhysicalBody
     {
     public:
+        VehicleChassis();
+        ~VehicleChassis();
+        void Render( sf::RenderTarget& target );
+        void Update( const sf::Time& dt );
+        void LoadFromPrototype( const VehicleChassisPrototype& prototype );
+
     private:
+        sf::Vector2f m_position = { 0.0f, 0.0f };
+        float m_direction = 0.0f;
+        std::vector< std::unique_ptr< Game::Graphics::Polygon > > m_shape;
+        std::vector< sf::Vector2f > m_physicalBodyShape;
+        sf::Vector2f m_pivot;
+        sf::Vector2f m_gunRotatorSlot;
     };
 }
